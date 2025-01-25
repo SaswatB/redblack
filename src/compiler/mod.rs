1eecb9259056b7f7c065698a0cfbf9a9c3d56186
Saswat Bhattacharya <sas.saswat@gmail.com>
2025-01-25 10:02:22 -0800

    add ast parser test

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,2 +1,3 @@
-pub mod types;
+pub mod checker;
 pub mod diagnostic_information_map_generated;
+pub mod types;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #![allow(dead_code)]
-#[allow(non_snake_case)] 
+#[allow(non_snake_case)]
+#[allow(non_upper_case_globals)]
 mod compiler;
+use std::{fs, path::Path};
+
+use oxc::allocator::Allocator;
+use oxc::parser::{ParseOptions, Parser};
+use oxc::span::SourceType;
 
 use crate::compiler::diagnostic_information_map_generated::Diagnostics;
 
@@ -11,4 +17,31 @@ fn main() {
     println!("Category: {:?}", diag.category);
     println!("Key: {}", diag.key);
     println!("Message: {}", diag.message);
+
+    let name = "src/compiler/types.ts";
+    let path = Path::new(&name);
+    let source_text = fs::read_to_string(path).unwrap();
+    let source_type = SourceType::from_path(path).unwrap();
+
+    let allocator = Allocator::default();
+    let ret = Parser::new(&allocator, &source_text, source_type)
+        .with_options(ParseOptions {
+            parse_regular_expression: true,
+            ..ParseOptions::default()
+        })
+        .parse();
+    let mut program = ret.program;
+
+    println!("AST:");
+    println!("{program:#?}");
+
+    if ret.errors.is_empty() {
+        println!("Parsed Successfully.");
+    } else {
+        for error in ret.errors {
+            let error = error.with_source_code(source_text.clone());
+            println!("{error:?}");
+            println!("Parsed with Errors.");
+        }
+    }
 }
